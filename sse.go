59a485351ca4aadb57b797f17778e377e30e5bd5
laurent buhler <lbuhler@gmail.com>
2021-01-21 15:04:47 -0800

    Handle clients connect/disconnet

--- sse.go
+++ sse.go
@@ -1,6 +1,9 @@
 package sse
 
-import "log"
+import (
+	"log"
+	"net/http"
+)
 
 type SseServer struct {
 	// channel of messages to be pushed to clients
@@ -31,16 +34,57 @@ func NewSseServer() *SseServer {
 	return server
 }
 
+func (server *SseServer) Connect(rw http.ResponseWriter, req *http.Request) {
+	flusher, ok := rw.(http.Flusher)
+
+	if !ok {
+		http.Error(rw, "Streaming unsupported!", http.StatusInternalServerError)
+		return
+	}
+	// Set the headers related to event streaming.
+	rw.Header().Set("Access-Control-Allow-Origin", "*")
+	rw.Header().Set("Content-Type", "text/event-stream")
+	rw.Header().Set("Cache-Control", "no-cache")
+	rw.Header().Set("Connection", "keep-alive")
+
+	// create communication channel and register
+	c := make(chan []byte)
+	server.register <- c
+
+	// listen to the closing of the http connection via the CloseNotifier
+	notify := rw.(http.CloseNotifier).CloseNotify()
+	go func() {
+		<-notify
+		log.Println("connection closed")
+		server.unregister <- c
+	}()
+
+	for {
+		_, err := rw.Write(<-c)
+		if err != nil {
+			log.Println("%v", err)
+		}
+		flusher.Flush()
+	}
+	log.Println("done")
+}
+func (server *SseServer) broadcast(message []byte) {
+	for c := range server.clients {
+		c <- message
+	}
+}
 func (server *SseServer) listen() {
 	for {
 		select {
 		case c := <-server.messages:
 			log.Println("new message")
+			server.broadcast(c)
 		case c := <-server.register:
 			log.Println("new client")
+			server.clients[c] = true
 		case c := <-server.unregister:
 			log.Println("client leaving")
+			delete(server.clients, c)
 		}
 	}
-
 }
